d730220d45ddbef6e244f1ba1e1344e0af580bff
TheRufio <therufio228@gmail.com>
2025-09-27 14:37:14 +0300

    Try to interact DRF

--- twogether/main/models.py
+++ twogether/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class CoupleGroup(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+class Gallery(models.Model):
+    SPECIAL_DAYS = [
+        ('BIRTHDAY', 'Birthday'),
+        # годовщина и тд
+    ]
+    
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    special_day = models.CharField()
--- twogether/main/serializers.py
+++ twogether/main/serializers.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from main.models import CoupleGroup
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fieds = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'email', 'couple_groups']
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        field = ['url', 'name']
+        fields = ['url', 'name']
+
+class CoupleGroupSerializer(serializers.ModelSerializer):
+    days_together = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CoupleGroup
+        fields = ['id', 'name', 'users', 'start_date', 'days_together']
+
+    def get_days_together(self, obj):
+        return obj.days_together()
